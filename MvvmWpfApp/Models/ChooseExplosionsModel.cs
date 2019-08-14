f71375189b615c095cb1f6e59102c9414e0ecc8a
RacheliRap <rachelirap@gmail.com>
2019-08-14 23:05:36 +0300

    First commit

---
+++ MvvmWpfApp/Models/ChooseExplosionsModel.cs
@@ -0,0 +1,75 @@
+﻿using BE;
+using BL;
+using MvvmWpfApp.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MvvmWpfApp.Models
+{
+    public class ChooseExplosionsModel : INotifyPropertyChanged
+    {
+        private readonly IBl _bl = new FactoryBl().GetInstance();
+
+        private List<Event> _Events = new List<Event>();
+        public List<Event> Events
+        {
+            get { return _Events; }
+            set
+            {
+                _Events = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ChooseExplosionsModel()
+        {
+            GetEvents();
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += checkNewEvents;
+            worker.RunWorkerAsync();
+        }
+
+        private void checkNewEvents(object sender, DoWorkEventArgs doWorkEventArgs)
+        {
+            while (true)
+            {
+                GetEvents();
+
+                Thread.Sleep(5000);
+            }
+        }
+
+
+        public async Task<List<Explosion>> getExplosionsFromEvent(Event _event)
+        {
+            return await _bl.GetExplosions(explosion => explosion.Event.Id == _event.Id);
+        }
+
+        public void GetEvents()
+        {
+            if (_bl.GetEvents().Count > 0)
+            {
+                Events = _bl.GetEvents();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void UpdateExplotion(Explosion explosion)
+        {
+            _bl.UpdateExplosion(explosion);
+        }
+    }
+}
---
+++ MvvmWpfApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using log4net;
+using MvvmWpfApp.ViewModels;
+using MahApps.Metro.Controls;
+
+namespace MvvmWpfApp.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private static readonly ILog Log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        public MainViewModel MainViewModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainViewModel = new MainViewModel();
+            ReportFormView.ReportFormVm = MainViewModel.NewReportFormVm;
+            MapView.MapVm = MainViewModel.MapVm;
+            Title = "Emergency";
+            GraphView.GraphVm = MainViewModel.GraphVm;
+            ChooseExplosionsView.ChooseExplosionsVm = MainViewModel.ChooseExplosionsVm;
+
+            DataContext = MainViewModel;
+            Closing += MainView_Closing;
+        }
+
+        private void FlipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var flipview = ((FlipView)sender);
+            switch (flipview.SelectedIndex)
+            {
+                case 0:
+                    flipview.BannerText = "Emergency!";
+                    break;
+                case 1:
+                    flipview.BannerText = "Map!";
+                    break;
+                case 2:
+                    flipview.BannerText = "Statistics!";
+                    break;
+                case 3:
+                    flipview.BannerText = "About!";
+                    break;
+                case 4:
+                    flipview.BannerText = "Pictures!";
+                    break;
+            }
+        }
+
+        private void MainView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            /*
+                if (((MainViewModel)(this.DataContext)).Data.IsModified)
+                if (!((MainViewModel)(this.DataContext)).PromptSaveBeforeExit())
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            */
+            Log.Info("Closing App");
+        }
+
+        private void SelectedTabChange(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+
+            //GridCursor.Margin = new Thickness((150 * index), 0, 0, 0);
+
+            if (index == 0)
+            {
+                //#region toShow
+
+                //ReportFormView.Visibility = Visibility.Visible;
+                //MapView.Visibility = Visibility.Visible;
+
+                //#endregion
+
+                //#region toHide
+                //GraphView.Visibility = Visibility.Collapsed;
+                //ChooseExplosionsView.Visibility = Visibility.Collapsed;
+                //#endregion
+
+            }
+            else if (index == 1)
+            {
+                //#region toShow
+                //GraphView.Visibility = Visibility.Visible;
+                //ChooseExplosionsView.Visibility = Visibility.Visible;
+                //#endregion
+
+                //#region toHide
+
+                //ReportFormView.Visibility = Visibility.Collapsed;
+                //MapView.Visibility = Visibility.Collapsed;
+
+                //#endregion
+            }
+        }
+
+        private void Map_Click(object sender, RoutedEventArgs e)
+        {
+            FlipViewTest.SelectedIndex = 1;
+        }
+        private void Stat_Click(object sender, RoutedEventArgs e)
+        {
+            FlipViewTest.SelectedIndex = 2;
+        }
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            FlipViewTest.SelectedIndex = 3;
+        }
+
+        private void pic_Click(object sender, RoutedEventArgs e)
+        {
+            FlipViewTest.SelectedIndex = 4;
+        }
+
+        private void HomeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FlipViewTest.SelectedIndex =0;
+        }
+
+        private void updateGraph(object sender, EventArgs e)
+        {
+            MainViewModel.GraphVm = new GraphVM();
+            GraphView.GraphVm = MainViewModel.GraphVm;
+            GraphView.DataContext = GraphView.GraphVm;
+        }
+    }
+}
